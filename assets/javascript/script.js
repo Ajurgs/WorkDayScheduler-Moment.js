46abbcea1db50c912d1407952debf63d6ab49f60
Ajurgs <ajjurgs5954@gmail.com>
2021-04-27 17:09:38 -0600

    added the basic structures for the time blocks and add them with a loop and jQuery

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,40 @@
 // variable declerations
-const currentDay = document.querySelector("#currentDay");
-
+const currentDay = $("#currentDay");
+const timeContainer = $(".container");
 // call fucntions
 showCurentDay();
+
+displayHours();
 
 // Functions ---------------------
 
 // set the current day in the header
 function showCurentDay() {
-  currentDay.textContent = `Today is ${moment().format("MMM Do YY")}`;
+  currentDay.text(`Today is ${moment().format("MMMM Do YYYY")}`);
 }
+
+// display all business hour time slots
+function displayHours() {
+  for (i = 0; i < 9; i++) {
+    let hour = 9 + i;
+    // set the html of the new time block div and use a ternary operator to determine AM and PM
+    let newChild = $('<div class="time-block"></div>').html(
+      `<div class="row">
+       <div class="hour">
+       ${hour > 12 ? hour - 12 + " PM" : hour + " AM"} 
+       </div>
+      <div class="present">placeholder</div>
+      <div class="saveBtn">save</div>
+      </div>`
+    );
+    timeContainer.append(newChild);
+  }
+}
+
+// on click enter the time blocks textbox
+// get user input for the block
+
+// on click save
+// save the current textbox
+
+// get the local saved information
