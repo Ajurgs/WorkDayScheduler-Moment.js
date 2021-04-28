e9a1fd8c5302af9399d0590956a6b77f6ad4a281
Ajurgs <ajjurgs5954@gmail.com>
2021-04-27 19:39:00 -0600

    set up the save and load schedual

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,20 @@
-// variable declerations
+// variable declerations ----------
 const currentDay = $("#currentDay");
 const timeContainer = $(".container");
-// call fucntions
+
+const cureentHour = moment().hour();
+
+let schedual = JSON.parse(localStorage.getItem("schedual")) || []; // return the stored schedual or a blank array if storage is empty
+
+// call fucntions -----------------
 showCurentDay();
 
 displayHours();
-
+getSavedSchedual();
+// event Listeners ---------
+timeContainer.on("click", ".saveBtn", function (event) {
+  saveSchedual(event);
+});
 // Functions ---------------------
 
 // set the current day in the header
@@ -20,10 +29,10 @@ function displayHours() {
     // set the html of the new time block div and use a ternary operator to determine AM and PM
     let newChild = $('<div class="time-block"></div>').html(
       `<div class="row">
-       <div class="hour">
+       <div class="hour" time = "${hour}">
        ${hour > 12 ? hour - 12 + " PM" : hour + " AM"} 
        </div>
-      <textarea class="present" cols="50" rows ="3"></textarea>
+      <textarea class="decription present" cols="50" rows ="3"></textarea>
       <button class="saveBtn">save</button>
       </div>`
     );
@@ -32,6 +41,31 @@ function displayHours() {
 }
 
 // on click save
+function saveSchedual(event) {
+  //create the object to store
+  let timeVal = $(event.target).closest(".row").find(".hour").attr("time");
+  let textVal = $(event.target).closest(".row").find(".decription").val();
+  let newItem = { time: timeVal, text: textVal };
+  // remove duplicate entries
+  schedual = schedual.filter((item) => item.time != newItem.time);
+  // add to the schedual array
+  schedual.push(newItem);
+
+  // save to local storage
+  localStorage.setItem("schedual", JSON.stringify(schedual));
+}
+
 // save the current textbox
 
 // get the local saved information
+function getSavedSchedual() {
+  $.each(schedual, function (index, value) {
+    // select the time atribute from time property of the the object
+    $(`[time='${value.time}']`)
+      .closest(".row")
+      .find(".decription")
+      .val(value.text);
+    // traverse to the associated textarea
+    // set the textfeild val to the text property
+  });
+}
